d80f9c3d0a55b97b366b96f5bdff72c77868443e
Isaac Vorköper <isaaclennart@gmail.com>
2020-11-10 14:51:05 +0100

    Finished the converting part.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -25,14 +25,55 @@ namespace Hastighed
             InitializeComponent();
         }
 
-        private void kmBox_TextChanged(object sender, TextChangedEventArgs e)
+        private void kmtBox_TextChanged(object sender, TextChangedEventArgs e)
         {
-
+            if (kmtBox.Text == "")
+            {
+                msBox.Text = "";
+            }
+            else
+            {
+                try
+                {
+                    msBox.Text = (double.Parse(kmtBox.Text) * 3.6).ToString();
+                    msBox.CaretIndex = msBox.Text.Length;
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("Input skal være et gyldigt tal");
+                }
+            }
         }
 
         private void msBox_TextChanged(object sender, TextChangedEventArgs e)
         {
+            if (msBox.Text == "")
+            {
+                kmtBox.Text = "";
+            }
+            else
+            {
+                try
+                {
+                    kmtBox.Text = (double.Parse(msBox.Text) / 3.6).ToString() + " km/t";
+                    kmtBox.CaretIndex = msBox.Text.Length;
+                }
+                catch (Exception)
+                {
 
+                    MessageBox.Show("Input skal være et gydigt tal");
+                }
+            }
+        }
+
+        private void msBox_GotKeyboardFocus(object sender, KeyboardFocusChangedEventArgs e)
+        {
+            msBox.Clear();
+        }
+
+        private void kmtBox_GotKeyboardFocus(object sender, KeyboardFocusChangedEventArgs e)
+        {
+            kmtBox.Clear();
         }
     }
 }
